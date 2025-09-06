0ac0b844b6bce0faffa919b4b2e35a87aee6cfc6
Vasily Mironov <whilesocold@gmail.com>
2025-09-07 01:52:32 +0300

    add Build.bat

---
+++ Build.bat
@@ -0,0 +1,2 @@
+C:\Work\UE\UE_5.6\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -plugin=C:\Work\UE\ALS-Community\ALSV4_CPP.uplugin -package=C:\Work\UE\Rougelike\Plugins\ALS set:HostPlatformOnly=true
+pause
